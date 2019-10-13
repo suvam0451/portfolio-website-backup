ee5b2acde4d417e02e183dd2f522a69e58b5168c
Debashish <suvam0451@gmail.com>
2019-10-14 01:27:56 +0530

    pre coloring done

--- website-source/gatsby-browser.js
+++ website-source/gatsby-browser.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+//import "./src/styles/style01.css"
+require("prismjs/themes/prism-tomorrow.css")
+//require("./src/css/prism.css")
+require(`prismjs/prism.js`)
+require(`prismjs/components/prism-clike.js`)
+require(`prismjs/components/prism-c.js`)
+require(`prismjs/components/prism-cpp.js`)
+require(`prismjs/components/prism-git.js`)

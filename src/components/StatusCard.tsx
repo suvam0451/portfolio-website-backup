5ca431e4c36835203023413edad445d35b8a7143
Debashish <suvam0451@gmail.com>
2020-01-10 17:34:05 +0530

    release for bigyanshu

--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -1,12 +1,23 @@
 import { Link } from "gatsby";
 import React, { Component } from "react";
 import { Col } from "reactstrap";
-import { Card } from "@blueprintjs/core";
+import {
+	Alignment,
+	Button,
+	Intent,
+	Breadcrumbs,
+	IBreadcrumbProps,
+	Icon,
+	Card,
+	NavbarHeading,
+	NavbarDivider,
+	NavbarGroup,
+	Classes,
+} from "@blueprintjs/core";
 
 function StatusCard() {
 	return (
-		<Col xs="12" sm="12" md="12" lg="12" xl="12">
-			<Card>
+			<Card className="w-full">
 				<h4>Git basics</h4>
 				<li>Full references</li>
 				<li>Discussions Available</li>
@@ -14,7 +25,6 @@ function StatusCard() {
 					<b>Status</b>: not deployed
 				</li>
 			</Card>
-		</Col>
 	);
 }
 

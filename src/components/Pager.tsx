fcc1b9135ef20fc19829e3edf4182c28da46a815
Debashish Patra <suvam0451@protonmail.com>
2021-02-07 18:53:34 +0530

    chore: remove all other unused component/mdx files

--- src/components/Pager.tsx
+++ src/components/Pager.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from '@emotion/styled'
 import { Link } from 'gatsby'
 
 interface PagerProps {
@@ -12,24 +11,9 @@ function pageUrl(prefix: string, page: number): string {
     return page <= 1 ? `/${prefix}` : `/${prefix}/${page}`;
 }
 
-const NavLink = styled(Link)`
-  background-color: #ff5700;
-  border-radius: 3px;
-  color: #fff;
-  font-family: sans-serif;
-  margin: 0 1rem 0 0;
-  padding: 0.25rem 0.5rem;
-  text-decoration: none;
-
-  &:active,
-  &:hover {
-    color: #fff;
-  }
-`
-
 export const Pager = ({prefix, page, total}: PagerProps) => (
-    <div>
-        {page > 1 && <NavLink to={pageUrl(prefix, page -1)}>Previous</NavLink>}
-        {page < total && <NavLink to={pageUrl(prefix, page+1)}>Next</NavLink>}
-    </div>
+    // <div>
+    //     {page > 1 && <a to={pageUrl(prefix, page -1)}>Previous</a>}
+    //     {page < total && <a to={pageUrl(prefix, page+1)}>Next</a>}
+    // </div>
 )
